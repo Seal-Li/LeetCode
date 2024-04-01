e3ba4424265de7934fcd45ca3f8a993979a748e7
李海豹 <hbli@tsingtao.com.cn>
2024-04-01 09:52:32 +0800

    [update] add linklist has circle or not

---
+++ interviews/others/cycleLinklist.py
@@ -0,0 +1,29 @@
+class ListNode(object):
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def has_circle(head):
+    if head is None:
+        return False
+    
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+head.next.next.next.next.next = ListNode(6)
+head.next.next.next.next.next.next = ListNode(7)
+head.next.next.next.next.next.next.next = ListNode(8)
+head.next.next.next.next.next.next.next.next = head.next.next
+
+print(has_circle(head))
+
